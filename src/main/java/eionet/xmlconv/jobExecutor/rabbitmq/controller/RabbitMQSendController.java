e5c6cd07695805ff54ebef39ff984f4db13570ea
Maria Favva <maria.favva@gmail.com>
2020-11-19 11:56:47 +0200

    removed unused import

--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/RabbitMQSendController.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/RabbitMQSendController.java
@@ -1,7 +1,6 @@
 package eionet.xmlconv.jobExecutor.rabbitmq.controller;
 
 import eionet.xmlconv.jobExecutor.rabbitmq.service.RabbitMQSender;
-import eionet.xmlconv.jobExecutor.rabbitmq.service.RabbitMQSenderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
