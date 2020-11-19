36c0632935374932d1b9cc069a49a8e562315ea5
Maria Favva <maria.favva@gmail.com>
2020-11-19 15:27:53 +0200

    test controller

--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/TestController.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/TestController.java
@@ -2,6 +2,7 @@ package eionet.xmlconv.jobExecutor.rabbitmq.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,11 @@ public class TestController {
 
     Logger logger = LoggerFactory.getLogger(TestController.class);
 
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
     @GetMapping("/con")
     public void test() {
-        logger.info("test connection");
+        logger.info("test connection for host: " + host);
     }
 }
