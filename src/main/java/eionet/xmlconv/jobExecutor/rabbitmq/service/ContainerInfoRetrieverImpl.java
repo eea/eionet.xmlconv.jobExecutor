768385456d385a152d26e0e702c64f26f83ebf9f
Maria Favva <maria.favva@gmail.com>
2020-12-01 16:43:23 +0200

    reverted changes. Refs#125141

--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/service/ContainerInfoRetrieverImpl.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/service/ContainerInfoRetrieverImpl.java
@@ -22,9 +22,6 @@ public class ContainerInfoRetrieverImpl implements ContainerInfoRetriever {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", "application/json");
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        System.out.println("call rancher-metadata");
-//        Object resultObject = restTemplate.getForObject("http://rancher-metadata/2015-12-19/self/container", Object.class);
-//        System.out.println(resultObject);
         Object result = restTemplate.exchange("http://rancher-metadata/2015-12-19/self/container", HttpMethod.GET, entity, Object.class);
         System.out.println(result);
         return result;
