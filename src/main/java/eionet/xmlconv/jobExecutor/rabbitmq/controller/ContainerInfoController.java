38758d8f3b34f6ae057876b06ffb1ef121b491c7
Maria Favva <maria.favva@gmail.com>
2020-12-01 13:09:36 +0200

    test new endpoint. Refs#125141

--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/ContainerInfoController.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/ContainerInfoController.java
@@ -1,27 +1,27 @@
-package eionet.xmlconv.jobExecutor.rabbitmq.controller;
-
-import eionet.xmlconv.jobExecutor.rabbitmq.service.ContainerInfoRetriever;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/container")
-public class ContainerInfoController {
-
-    private ContainerInfoRetriever containerInfoRetriever;
-
-    @Autowired
-    public ContainerInfoController(ContainerInfoRetriever containerInfoRetriever) {
-        this.containerInfoRetriever = containerInfoRetriever;
-    }
-
-    @GetMapping("/info")
-    public String getInfo() {
-        System.out.println("inside method test");
-        Object result = containerInfoRetriever.getContainerId();
-        System.out.println(result);
-        return "info retrieved";
-    }
-}
+//package eionet.xmlconv.jobExecutor.rabbitmq.controller;
+//
+//import eionet.xmlconv.jobExecutor.rabbitmq.service.ContainerInfoRetriever;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/container")
+//public class ContainerInfoController {
+//
+//    private ContainerInfoRetriever containerInfoRetriever;
+//
+//    @Autowired
+//    public ContainerInfoController(ContainerInfoRetriever containerInfoRetriever) {
+//        this.containerInfoRetriever = containerInfoRetriever;
+//    }
+//
+//    @GetMapping("/info")
+//    public String getInfo() {
+//        System.out.println("inside method test");
+//        Object result = containerInfoRetriever.getContainerId();
+//        System.out.println(result);
+//        return "info retrieved";
+//    }
+//}
--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/RabbitMQSendController.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/controller/RabbitMQSendController.java
@@ -1,17 +1,18 @@
 package eionet.xmlconv.jobExecutor.rabbitmq.controller;
 
+import eionet.xmlconv.jobExecutor.rabbitmq.service.ContainerInfoRetriever;
 import eionet.xmlconv.jobExecutor.rabbitmq.service.RabbitMQSender;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rabbitmq")
 public class RabbitMQSendController {
 
     RabbitMQSender rabbitMQSender;
+
+    @Autowired
+    ContainerInfoRetriever containerInfoRetriever;
 
     @Autowired
     public RabbitMQSendController(RabbitMQSender rabbitMQSender) {
@@ -22,5 +23,13 @@ public class RabbitMQSendController {
     public String sendMessage(@PathVariable String message) {
         rabbitMQSender.sendMessage(message);
         return "message sent";
+    }
+
+    @GetMapping("/info")
+    public String getInfo() {
+        System.out.println("inside method test");
+        Object result = containerInfoRetriever.getContainerId();
+        System.out.println(result);
+        return "info retrieved";
     }
 }
