9b77dda8b73bd85f020ad723a57a3e1a64e52712
Maria Favva <maria.favva@gmail.com>
2020-12-09 13:54:11 +0200

    manage exception during rancher metadata retrieval. Refs#125141

--- src/main/java/eionet/xmlconv/jobExecutor/rancher/service/ContainerInfoRetrieverImpl.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rancher/service/ContainerInfoRetrieverImpl.java
@@ -1,8 +1,11 @@
 package eionet.xmlconv.jobExecutor.rancher.service;
 
 import eionet.xmlconv.jobExecutor.rancher.entity.ContainerInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,25 +13,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+
 @Service
 public class ContainerInfoRetrieverImpl implements ContainerInfoRetriever {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContainerInfoRetrieverImpl.class);
 
     @Value("${rancher.metadata.container.url}")
     private String rancherMetadataUrl;
 
     private RestTemplate restTemplate;
+    private Environment env;
+
+    private static final String DEV_PROFILE = "dev";
+    private static final String DEMO_CONTAINER_NAME = "demoJobExecutor";
 
     @Autowired
-    public ContainerInfoRetrieverImpl(RestTemplate restTemplate) {
+    public ContainerInfoRetrieverImpl(RestTemplate restTemplate, Environment env) {
         this.restTemplate = restTemplate;
+        this.env = env;
     }
 
     @Override
     public String getContainerName() {
+        boolean devProfile = Arrays.asList(env.getActiveProfiles()).stream().allMatch(p -> p.equals(DEV_PROFILE));
+        if (devProfile) {
+            return DEMO_CONTAINER_NAME;
+        }
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", "application/json");
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<ContainerInfo> result = restTemplate.exchange(rancherMetadataUrl, HttpMethod.GET, entity, ContainerInfo.class);
+        ResponseEntity<ContainerInfo> result;
+        try {
+            result = restTemplate.exchange(rancherMetadataUrl, HttpMethod.GET, entity, ContainerInfo.class);
+        } catch (Exception e) {
+            LOGGER.info("Error retrieving rancher metadata");
+            return "";
+        }
         ContainerInfo containerInfo = result.getBody();
         return containerInfo.getName();
     }
