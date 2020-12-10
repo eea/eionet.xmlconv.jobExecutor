d6cd5e895134594659aa80ea4dc9de530f7126f8
deniaAthanasakou <deniaAthanasakou>
2020-12-10 16:46:03 +0200

    Added container name to the messages. Refs #125093

--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/listener/RabbitMQListener.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/listener/RabbitMQListener.java
@@ -4,6 +4,7 @@ import eionet.xmlconv.jobExecutor.exceptions.RabbitMQException;
 import eionet.xmlconv.jobExecutor.exceptions.ScriptExecutionException;
 import eionet.xmlconv.jobExecutor.objects.Script;
 import eionet.xmlconv.jobExecutor.rabbitmq.service.RabbitMQSender;
+import eionet.xmlconv.jobExecutor.rancher.service.ContainerInfoRetriever;
 import eionet.xmlconv.jobExecutor.scriptExecution.services.ScriptExecutionService;
 import eionet.xmlconv.jobExecutor.scriptExecution.services.impl.ScriptExecutionServiceImpl;
 import org.slf4j.Logger;
@@ -13,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.apache.commons.lang.time.StopWatch;
 
-import java.io.IOException;
 import eionet.xmlconv.jobExecutor.Constants;
 import eionet.xmlconv.jobExecutor.Properties;
 
@@ -21,20 +21,23 @@ import eionet.xmlconv.jobExecutor.Properties;
 @Component
 public class RabbitMQListener {
 
-    ScriptExecutionService ses;
-    RabbitMQSender rabbitMQSender;
+    private ScriptExecutionService ses;
+    private RabbitMQSender rabbitMQSender;
+    private ContainerInfoRetriever containerInfoRetriever;
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQListener.class);
 
     @Autowired
-    public RabbitMQListener(RabbitMQSender rabbitMQSender) {
+    public RabbitMQListener(RabbitMQSender rabbitMQSender, ContainerInfoRetriever containerInfoRetriever) {
         this.rabbitMQSender = rabbitMQSender;
+        this.containerInfoRetriever = containerInfoRetriever;
     }
 
     @RabbitListener(queues = "${job.rabbitmq.listeningQueue}")
     public void consumeMessage(Script script) throws RabbitMQException{
         LOGGER.info("Received script with id " + script.getJobId());
+        String containerName = containerInfoRetriever.getContainerName();
 
-        String jobReceivedMessage = Properties.getMessage(Constants.WORKER_LOG_JOB_RECEIVED, new String[] {script.getJobId()});
+        String jobReceivedMessage = Properties.getMessage(Constants.WORKER_LOG_JOB_RECEIVED, new String[] {containerName, script.getJobId()});
         rabbitMQSender.sendMessage(jobReceivedMessage);
 
         ses = new ScriptExecutionServiceImpl(script);
@@ -44,11 +47,11 @@ public class RabbitMQListener {
         try{
             ses.getResult();
             timer.stop();
-            replyMessage = Properties.getMessage(Constants.WORKER_LOG_JOB_SUCCESS, new String[] {script.getJobId(), timer.toString()});
+            replyMessage = Properties.getMessage(Constants.WORKER_LOG_JOB_SUCCESS, new String[] {containerName, script.getJobId(), timer.toString()});
         }
         catch(ScriptExecutionException e){
             timer.stop();
-            replyMessage = Properties.getMessage(Constants.WORKER_LOG_JOB_FAILURE, new String[] {script.getJobId(), timer.toString()}) + " Error message: " + e.getMessage();
+            replyMessage = Properties.getMessage(Constants.WORKER_LOG_JOB_FAILURE, new String[] {containerName, script.getJobId(), timer.toString()}) + " Error message: " + e.getMessage();
         }
         finally {
             rabbitMQSender.sendMessage(replyMessage);
