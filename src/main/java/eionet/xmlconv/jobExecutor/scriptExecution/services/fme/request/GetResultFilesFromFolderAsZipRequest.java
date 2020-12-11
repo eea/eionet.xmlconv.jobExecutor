0946965f7c01d713ade448e7c6b03ddd76352338
deniaAthanasakou <deniaAthanasakou>
2020-12-11 15:26:27 +0200

    Small Fix. Refs #125093

--- src/main/java/eionet/xmlconv/jobExecutor/scriptExecution/services/fme/request/GetResultFilesFromFolderAsZipRequest.java
+++ src/main/java/eionet/xmlconv/jobExecutor/scriptExecution/services/fme/request/GetResultFilesFromFolderAsZipRequest.java
@@ -1,4 +1,4 @@
-wpackage eionet.xmlconv.jobExecutor.scriptExecution.services.fme.request;
+package eionet.xmlconv.jobExecutor.scriptExecution.services.fme.request;
 import eionet.xmlconv.jobExecutor.Properties;
 import org.json.JSONArray;
 import org.json.JSONObject;
