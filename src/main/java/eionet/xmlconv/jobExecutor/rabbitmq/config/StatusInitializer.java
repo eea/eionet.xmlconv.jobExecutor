4efe0d04fdbbc917f7622c123e2ab55cb74f4b08
Maria Favva <maria.favva@gmail.com>
2021-02-18 19:12:40 +0200

    Renamed method for initializing worker status and added logger. Refs#128109

--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/config/StatusInitializer.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/config/StatusInitializer.java
@@ -3,6 +3,8 @@ package eionet.xmlconv.jobExecutor.rabbitmq.config;
 import eionet.xmlconv.jobExecutor.rabbitmq.model.WorkersRabbitMQResponse;
 import eionet.xmlconv.jobExecutor.rabbitmq.service.RabbitMQSender;
 import eionet.xmlconv.jobExecutor.rancher.service.ContainerInfoRetriever;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,14 +15,17 @@ public class StatusInitializer {
     private RabbitMQSender rabbitMQSender;
     private ContainerInfoRetriever containerInfoRetriever;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StatusInitializer.class);
+
     public StatusInitializer(RabbitMQSender rabbitMQSender, ContainerInfoRetriever containerInfoRetriever) {
         this.rabbitMQSender = rabbitMQSender;
         this.containerInfoRetriever = containerInfoRetriever;
     }
 
     @EventListener(ApplicationReadyEvent.class)
-    public void doSomethingAfterStartup() {
+    public void initializeWorkerStatusAfterStartup() {
         WorkersRabbitMQResponse response = new WorkersRabbitMQResponse().setContainerName(containerInfoRetriever.getContainerName()).setJobExecutorStatus(1);
         rabbitMQSender.sendMessage(response);
+        LOGGER.info("Message for initializing JobExecutor status sent on rabbitmq");
     }
 }
