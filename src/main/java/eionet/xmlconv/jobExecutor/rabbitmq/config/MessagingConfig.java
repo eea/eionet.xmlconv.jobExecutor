bb94164e582ad74d5ff1aab6fb1b349c094c0d43
Maria Favva <maria.favva@gmail.com>
2021-02-22 09:50:52 +0200

    Configuration for retrieving only one message a time. Refs#128109

--- src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/config/MessagingConfig.java
+++ src/main/java/eionet/xmlconv/jobExecutor/rabbitmq/config/MessagingConfig.java
@@ -1,12 +1,8 @@
 package eionet.xmlconv.jobExecutor.rabbitmq.config;
 
 import eionet.xmlconv.jobExecutor.Constants;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -22,18 +18,10 @@ public class MessagingConfig {
     }
 
     @Bean
-    public RabbitListenerContainerFactory<SimpleMessageListenerContainer> prefetchTenRabbitListenerContainerFactory(ConnectionFactory rabbitConnectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(rabbitConnectionFactory);
-        factory.setPrefetchCount(1);
-        return factory;
-    }
-
-    @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-       final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-       rabbitTemplate.setMessageConverter(jsonMessageConverter());
-       return rabbitTemplate;
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 
     @Bean
