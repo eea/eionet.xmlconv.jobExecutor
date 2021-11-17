265f246b7f4496da9c7284caf7636daf6fc45265
deniaAthanasakou <antonia.ath@gmail.com>
2021-11-17 14:57:18 +0200

    Added FME method to construct folder of results.

--- src/main/java/eionet/xmlconv/jobExecutor/scriptExecution/services/fme/FMEUtils.java
+++ src/main/java/eionet/xmlconv/jobExecutor/scriptExecution/services/fme/FMEUtils.java
@@ -1,8 +1,35 @@
 package eionet.xmlconv.jobExecutor.scriptExecution.services.fme;
 
-public class FMEUtils {
+import eionet.xmlconv.jobExecutor.utils.Utils;
 
-    public void constructErrorResultAsHmtlInsideZip(String errorResult){
+public final class FMEUtils {
 
+    public static String createErrorMessage (String fmeJobId, String scriptUrl, String sourceUrl, String exception){
+        String resultStringHtml = "<div class=\"feedbacktext\"><span id=\"feedbackStatus\" class=\"BLOCKER\" style=\"display:none\">";
+        String resultStringMsg ="The QC Process failed, please allow some time and re-run the process. If the issue persists please contact the dataflow helpdesk. ";
+        String resultStringSpecificMsg;
+        if (Utils.isNullStr(fmeJobId)){
+            resultStringSpecificMsg = "Job submission for script: " + scriptUrl + " and xml url " + sourceUrl + " has failed. ";
+        }
+        else{
+            resultStringSpecificMsg = "The id in the FME server is #" + fmeJobId + ". ";
+        }
+        String exceptionMsg = "Exception message is: " + exception;
+        String fullResultString = resultStringHtml + resultStringMsg + resultStringSpecificMsg + exceptionMsg +  "</span>" ;
+        fullResultString += resultStringMsg + resultStringSpecificMsg + exceptionMsg + "</div>";
+        return fullResultString;
+    }
+
+    public static String constructFMEFolderName(String xmlFileUrl, String randomStr){
+        String[] urlSegments = xmlFileUrl.split("/");
+        String fileNameWthXml = urlSegments[urlSegments.length-1];
+        String[] fileNameSegments = fileNameWthXml.split("\\.");
+        String fileName = fileNameSegments[0];
+        //if a cdr/bdr folder url was provided, get the folder name
+        if(fileName.equals("xml") && (urlSegments.length-2>=0)){
+            fileName = urlSegments[urlSegments.length-2];
+        }
+        String folderName = fileName + "_" +  randomStr;
+        return folderName;
     }
 }
